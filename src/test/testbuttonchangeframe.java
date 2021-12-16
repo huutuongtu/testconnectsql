26ff1d85548c795abbd739ab5d8257aa30af40ff
Huu Tuong Tu <83907151+huutuongtu@users.noreply.github.com>
2021-12-16 22:51:10 +0700

    testchange

--- src/test/testbuttonchangeframe.java
+++ src/test/testbuttonchangeframe.java
@@ -5,48 +5,66 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
-
+import javax.swing.JOptionPane;
 public class testbuttonchangeframe {
-    JFrame f;
+
 
     testbuttonchangeframe() {
-        JPanel p = new JPanel();
-        p.setLayout(new BorderLayout());
-        p.setSize(500,300);
-        f = new JFrame();
-
-
+        JPanel f;
+        JFrame fr = new JFrame();
+        fr.setUndecorated(true);
+        fr.setSize(600,700);
+        f = new JPanel();
+        f.setSize(500, 300);
         JButton bu = new JButton("123");
         bu.setSize(20,20);
         f.add(bu);
+        fr.add(f);
+        fr.setVisible(true);
 
         bu.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                f.setVisible(false);
+                JPanel jp = new JPanel();
+                jp.setLayout(new FlowLayout());
+                jp.setSize(100,20);
+                JPanel jp2 = new JPanel();
+                jp2.setSize(100,20);
+                jp2.setLayout(new FlowLayout());
                 JFrame jf = new JFrame();
-                JButton but = new JButton();
-                but.setSize(20,30);
-                jf.add(but);
+                jf.setExtendedState(jf.MAXIMIZED_BOTH);
+                jf.setLayout(new FlowLayout());
+                JButton but = new JButton("123");
+                JButton butt = new JButton("345");
+                JButton buttt = new JButton("567");
+                butt.setSize(20,20);
+                but.setSize(20,20);
+                jf.setSize(200,100);
+                jp.add(but);
+                jp.add(butt);
+                jp2.add(buttt);
+                jf.add(jp2);
+                jf.add(jp);
+                fr.setVisible(false);
+                jf.setVisible(true);
+                butt.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        JOptionPane.showMessageDialog(null,"ấn làm j");
+                    }
+                });
                 but.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         jf.setVisible(false);
-                        f.setVisible(true);
+                        fr.setVisible(true);
                     }
                 });
-                jf.setVisible(true);
-                jf.setSize(200,300);
             }
         });
-
-
-        f.setSize(500, 300);
-        f.setVisible(true);
-        p.add(f);
     }
 
     public static void main(String[] args) {
-        new testbuttonchangeframe();
+        new testbuttonchangeframe(); 
     }
 }
